381187b6260a6c65ae875ae3336aac1f5f7af0f4
manprew <dimas.khairurrahman@gmail.com>
2023-09-02 21:00:35 +0700

    Create contoh1.php

---
+++ Tugas1/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Dimas Khairurrahman
+        Saya tinggal di Daerah Tanjungpura
+        Olahraga yang saya sukai adalah
+        Renang";
+    }
+    
+
+}
